46524e55a7cf024e5d8f61c12cb4d1196be63ef6
mboyea <contact@mboyea.com>
2024-11-01 16:38:20 -0500

    tried to get fly ssh console working but didn't work

--- src/server-image.nix
+++ src/server-image.nix
@@ -1,6 +1,19 @@
-{ lib, pkgs, name, version, packages }: pkgs.dockerTools.streamLayeredImage {
+{ lib, pkgs, name, version, packages }: let
+  os = pkgs.dockerTools.pullImage {
+    imageName = "nixos/nix";
+    imageDigest =
+      "sha256:473a2b527958665554806aea24d0131bacec46d23af09fef4598eeab331850fa";
+    finalImageName = "nix";
+    finalImageTag = "2.11.1";
+    sha256 = "sha256-qvhj+Hlmviz+KEBVmsyPIzTB3QlVAFzwAY1zDPIBGxc=";
+    os = "linux";
+    arch = "x86_64";
+  };
+in pkgs.dockerTools.streamLayeredImage {
   name = "${name}-server-image";
   tag = "${version}";
+  # fromImage = os;
+  # maxLayers = 125;
   contents = [
     packages.server
   ];
