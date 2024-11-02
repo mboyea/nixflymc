25e0122abab32692cb33062bf43b8b9cc3e2ca35
mboyea <contact@mboyea.com>
2024-11-02 12:02:10 -0500

    added alpine to image in hope ssh will work with a root user

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.podman
+          pkgs.nix-prefetch-docker
           pkgs.gzip
           pkgs.skopeo
           pkgs.flyctl
--- src/server-image.nix
+++ src/server-image.nix
@@ -1,19 +1,21 @@
 { lib, pkgs, name, version, packages }: let
-  os = pkgs.dockerTools.pullImage {
-    imageName = "nixos/nix";
-    imageDigest =
-      "sha256:473a2b527958665554806aea24d0131bacec46d23af09fef4598eeab331850fa";
-    finalImageName = "nix";
-    finalImageTag = "2.11.1";
-    sha256 = "sha256-qvhj+Hlmviz+KEBVmsyPIzTB3QlVAFzwAY1zDPIBGxc=";
+  base = pkgs.dockerTools.pullImage {
+    # to get variables to update base image:
+    # https://hub.docker.com/_/busybox/tags
+    # nix develop
+    # nix-prefetch-docker --quiet --image-name busybox --image-tag 1.37.0 --image-digest sha256:bd39d7ac3f02301aec35d27a633d643770c0c4073c5b8cb588b1680c4f1e84e5
+    imageName = "busybox";
+    imageDigest ="sha256:bd39d7ac3f02301aec35d27a633d643770c0c4073c5b8cb588b1680c4f1e84e5";
+    finalImageName = "busybox";
+    finalImageTag = "1.37.0";
+    sha256 = "1rwnka21y7rj8jz250ay7c6mmyja7nngl0ia1z64rbcxq4ylgdvi";
     os = "linux";
     arch = "x86_64";
   };
 in pkgs.dockerTools.streamLayeredImage {
   name = "${name}-server-image";
   tag = "${version}";
-  # fromImage = os;
-  # maxLayers = 125;
+  fromImage = base;
   contents = [
     packages.server
   ];
